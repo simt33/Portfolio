cd33fead3ceff610583130e1bc1c78174fc2a308
Simon <simt33@yahoo.co.uk>
2020-09-11 17:06:08 +0100

    Changed body from CharField to TextField

--- blog/models.py
+++ blog/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class BlogEntry(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateField(auto_now=True)
-    body = models.CharField(max_length=4000)
+    body = models.TextField()
     image = models.ImageField(upload_to='images')
