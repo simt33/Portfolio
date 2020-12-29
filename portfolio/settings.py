9bc593b9a1431f4d24fa70303be6b9eed6480c26
Simon <simt33@yahoo.co.uk>
2020-12-29 20:04:25 +0000

    Moved over static files to root directory and deleted inner static folder

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATICFILES_DIRS = [
-os.path.join(BASE_DIR, 'portfolio/static/')
+os.path.join(BASE_DIR, '/static/')
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
